10f33526da9d6f7ed0fcc432a57b420ccea959c8
diegoco97 <diegoco08@hotmail.com>
2021-03-15 19:34:06 -0500

    prueba 1

---
+++ src/main/java/co/edu/unicundi/ejerciciogitprueva/Modulo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.ejerciciogitprueva;
+
+/**
+ *
+ * @author diego
+ */
+public class Modulo {
+    
+    public static void main(String[] args) {
+        System.out.println("hola mundo!!!");
+    }
+}
